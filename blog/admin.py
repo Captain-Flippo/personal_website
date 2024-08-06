f0fef68e7d7b51cb3704a841f586787e99a13367
Captain-Flippo <captain_flippo@gmx.de>
2024-08-06 20:35:08 +0200

    add markdownx editor to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -7,15 +7,19 @@ class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
-class PostAdmin(admin.ModelAdmin):
-    pass
+# class PostAdmin(admin.ModelAdmin):
+#     pass
 
 
 class CommentAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Post)
+class BlogPostAdmin(MarkdownxModelAdmin):
+    list_display = ('title', 'created_on', 'last_modified')
+    search_fields = ('title', 'categories')
 
 admin.site.register(Category, CategoryAdmin)
 #admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
-admin.site.register(Post, MarkdownxModelAdmin)
+#admin.site.register(Post, MarkdownxModelAdmin)
